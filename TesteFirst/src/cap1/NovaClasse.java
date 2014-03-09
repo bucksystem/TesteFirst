9274ac874b6a303b51553219562974e8fb04e171
Rogerio Costa <rogerio.costa@bucksystem.com.br>
2014-03-08 22:28:44 -0300

    second commit

---
+++ TesteFirst/src/cap1/NovaClasse.java
@@ -0,0 +1,20 @@
+package cap1;
+
+public class NovaClasse {
+
+
+	private int numeroi;
+	private String nome;
+	private double valor;
+
+	public NovaClasse(int numeroi, String nome, double valor) {
+		this.numeroi = numeroi;
+		this.nome = nome;
+		this.valor = valor;
+	}
+
+    public String  getNome() {
+		return nome;
+	}
+
+}
--- TesteFirst/src/cap1/TesteFirst.java
+++ TesteFirst/src/cap1/TesteFirst.java
@@ -1,7 +1,15 @@
 package cap1;
 
 public class TesteFirst {
+
 	public static void main(String[] args) {
 		System.out.println("nome....");
+		
+		System.out.println("teste  teste  teste....");
+		
+		 NovaClasse novaClasse = new NovaClasse(1, "rogerio", 10);
+		 
+		 System.out.println(novaClasse.getNome());
+		
 	}
 }
